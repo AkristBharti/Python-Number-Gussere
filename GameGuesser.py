080633b7ff02a1379c86322d47ce23bd15252c7d
alirezaaxa <alireza.ahmadi.axa@gmail.com>
2021-11-02 11:41:58 -0400

    Checking user input

--- GameGuesser.py
+++ GameGuesser.py
@@ -1,11 +1,23 @@
-import random
+from random import randint
+
 print("You have 5 tries to guess the number correctly")
 guessed = False
 tries = 5
-n = random.randint(1,10)
+computer_gusse = randint(1,10)
+
 while (guessed == False and tries > 0):
-    userin = int(input("Guess a number between 1 to 9 : "))
-    if(n == userin):
+    # Checking the correctness of the input
+    try:
+        userin = int(input("Guess a number between 1 to 9 : "))
+    except ValueError:
+        print("Please use only integers ")
+        continue
+    # Check the guess
+    if userin < 0 or userin > 9:
+        print("Please guess between 1 and 9")
+        tries = tries -1
+        continue
+    elif(computer_gusse == userin):
         print("WOW, you guessed it correctly")
     else:
         print("Oof, wrong number try again")
